2d7baef8802a2536ae451d30bd3ca84b9bf34207
alpayOnal <alpaycom@gmail.com>
2016-08-25 15:39:56 +0300

    google place api integration

--- flj_android/app/src/main/java/com/muatik/flj/flj/UI/activities/PostJob.java
+++ flj_android/app/src/main/java/com/muatik/flj/flj/UI/activities/PostJob.java
@@ -1,19 +1,144 @@
 package com.muatik.flj.flj.UI.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.PlaceBuffer;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.muatik.flj.flj.R;
+import com.muatik.flj.flj.UI.adapters.PlacesAutoCompleteAdapter;
 
 public class PostJob extends DetailActivity {
+
+    private PlacePicker.IntentBuilder builder;
+    private PlacesAutoCompleteAdapter mPlacesAdapter;
+    private Button pickerBtn;
+    private AutoCompleteTextView myLocation;
+    private static final int PLACE_PICKER_FLAG = 1;
+
+    private static final LatLngBounds BOUNDS_GREATER_SYDNEY = new LatLngBounds(
+            new LatLng(41.0082376,28.9783589), new LatLng(42.0082376,28.9783589));
+    protected GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .build();
+
         setContentView(R.layout.post_job_activity);
         init();
 
         getSupportActionBar().setTitle(getResources().getString(R.string.post_job_activity_title));
 
+
+
+        builder = new PlacePicker.IntentBuilder();
+        myLocation = (AutoCompleteTextView) findViewById(R.id.myLocation);
+        mPlacesAdapter = new PlacesAutoCompleteAdapter(this, android.R.layout.simple_list_item_1,
+                mGoogleApiClient, BOUNDS_GREATER_SYDNEY, null);
+        myLocation.setOnItemClickListener(mAutocompleteClickListener);
+        myLocation.setAdapter(mPlacesAdapter);
+        pickerBtn = (Button) findViewById(R.id.pickerBtn);
+        pickerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    builder = new PlacePicker.IntentBuilder();
+                    Intent intent = builder.build(PostJob.this);
+                    // Start the Intent by requesting a result, identified by a request code.
+                    startActivityForResult(intent, PLACE_PICKER_FLAG);
+
+                } catch (GooglePlayServicesRepairableException e) {
+                    GooglePlayServicesUtil
+                            .getErrorDialog(e.getConnectionStatusCode(), PostJob.this, 0);
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    Toast.makeText(PostJob.this, "Google Play Services is not available.",
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+        });
+
     }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case PLACE_PICKER_FLAG:
+                    Place place = PlacePicker.getPlace(data, this);
+                    //googleplacesapitutorial V/Latitude is: 41.079413300000006
+                    ///Longitude is: 28.853854499999997
+                    LatLng latLng = place.getLatLng();
+                    Log.v("Latitude is", "" + latLng.latitude);
+                    Log.v("Latitude is", "" + latLng.longitude);
+                    TextView latlongtext = (TextView) findViewById(R.id.latlong);
+                    latlongtext.setText(latLng.toString());
+
+                    myLocation.setText(place.getName() + ", " + place.getAddress());
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    private AdapterView.OnItemClickListener mAutocompleteClickListener
+            = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            final PlacesAutoCompleteAdapter.PlaceAutocomplete item = mPlacesAdapter.getItem(position);
+            final String placeId = String.valueOf(item.placeId);
+            PendingResult<PlaceBuffer> placeResult = Places.GeoDataApi
+                    .getPlaceById(mGoogleApiClient, placeId);
+            placeResult.setResultCallback(mUpdatePlaceDetailsCallback);
+        }
+    };
+
+    private ResultCallback<PlaceBuffer> mUpdatePlaceDetailsCallback
+            = new ResultCallback<PlaceBuffer>() {
+        @Override
+        public void onResult(PlaceBuffer places) {
+            if (!places.getStatus().isSuccess()) {
+                Log.e("place", "Place query did not complete. Error: " +
+                        places.getStatus().toString());
+                return;
+            }
+            // Selecting the first object buffer.
+            final Place place = places.get(0);
+        }
+    };
 }
